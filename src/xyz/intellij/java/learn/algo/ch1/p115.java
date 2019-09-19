2eabb9aa122f01a818cd6df6335ddf675648e62a
whp98 <whp98@outlook.com>
2019-09-19 14:43:08 +0800

    使用if语句判断数值的范围

---
+++ src/xyz/intellij/java/learn/algo/ch1/p115.java
@@ -0,0 +1,15 @@
+package xyz.intellij.java.learn.algo.ch1;
+
+public class p115 {
+    public static void main(String[] args) {
+        double x,y;
+        x=0.8;
+        y=0;
+        //使用if语句判断数值的范围
+        if (x<1&&x>0&&y<1&&y>0) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
