c68228ddc7aa221e78336ec284bb41b6bb925a67
whp <whp98@outlook.com>
2019-11-26 23:56:43 +0800

    动态规划,矩阵链乘法

---
+++ src/xyz/intellij/DynamicProgramming/MatrixChainOrder.java
@@ -0,0 +1,68 @@
+package xyz.intellij.DynamicProgramming;
+
+import java.util.Scanner;
+
+public class MatrixChainOrder {
+    private static int n;
+    private static int[][] m = new int[100][100];
+    private static int[][] s = new int[100][100];
+    private static int[] p = new int[105];
+    private static final int MAX = Integer.MAX_VALUE;
+
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        n = scan.nextInt();
+        for (int i = 0; i <= n; i++)
+        {
+            p[i] = scan.nextInt();
+            m[i][i] = 0;
+        }
+
+        for (int l = 2; l <= n; l++)
+        {
+            for (int i = 1; i <= n - l + 1; i++)
+            {
+                int j = i + l - 1;
+                m[i][j] = MAX;
+                for (int k = i; k <= j - 1; k++)
+                {
+                    int q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
+                    if (q < m[i][j])
+                    {
+                        m[i][j] = q;
+                        s[i][j] = k;
+                    }
+                }
+            }
+        }
+        print(1, n);
+        System.out.printf(" %d\n", m[1][n]);
+        matrixPrint(s);
+        matrixPrint(m);
+    }
+
+    public static void print(int i, int j)
+    {
+        if (i == j)
+            System.out.print("A" + i);
+        else
+        {
+            System.out.print("(");
+            print(i, s[i][j]);
+            print(s[i][j] + 1, j);
+            System.out.print(")");
+        }
+    }
+    //定义一个矩阵输出法
+     static void matrixPrint(int[][] obj){
+        for (int i=0;i<=n;i++){
+            for (int j = 0;j<=n;j++){
+                if (i<=j){
+                    System.out.print(obj[i][j]+"\t");
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+}
